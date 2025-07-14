61855e2f968311aea9dcb925ef341261328a3606
Daniel Solis Salge <danisalgedev@gmail.com>
2025-07-14 11:31:42 -0300

    Start project

---
+++ escola/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from escola.models import Estudante, Curso
+
+class EstudanteAdmin(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+  list_display_links = ('id', 'nome')
+  search_fields = ('nome',)
+  list_per_page = 20
+
+class CursoAdmin(admin.ModelAdmin):
+  list_display = ('id', 'codigo', 'descricao', 'nivel')
+  list_display_links = ('id', 'codigo')
+  search_fields = ('codigo',)
+  list_per_page = 20
+
+admin.site.register(Estudante, EstudanteAdmin)
+admin.site.register(Curso, CursoAdmin)
---
+++ escola/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Estudante(models.Model):
+  nome = models.CharField(max_length=30)
+  email = models.EmailField(max_length=254, blank=False)
+  cpf = models.CharField(max_length=11)
+  data_nascimento = models.DateField()
+  celular = models.CharField(max_length=14)
+  
+  def __str__(self):
+    return self.nome
+  
+class Curso(models.Model):
+  NIVEL_CHOICES = (
+    ('B', 'Básico'),
+    ('I', 'Intermediário'),
+    ('A', 'Avançado'),
+  )
+  
+  codigo = models.CharField(max_length=10)
+  descricao = models.CharField(max_length=100, blank=False)
+  nivel = models.CharField(max_length=1, choices=NIVEL_CHOICES, blank=False, null=False, default='B')
+  
+  def __str__(self):
+    return self.codigo
