4bfa62fecd73d8ad488d8cf4bb92d42384bac267
Daniel Solis Salge <danisalgedev@gmail.com>
2025-07-16 09:32:30 -0300

    Add Matricula model, serializers, and admin interface; implement viewsets and update URL routing

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from escola.models import Estudante, Curso
-from escola.serializers import EstudanteSerializer, CursoSerializer
-from rest_framework import viewsets
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudantesSerializer, ListaMatriculasCursoSerializer
+from rest_framework import viewsets, generics
 
 class EstudantesViewSet(viewsets.ModelViewSet):
   queryset = Estudante.objects.all()
@@ -9,3 +9,19 @@ class EstudantesViewSet(viewsets.ModelViewSet):
 class CursosViewSet(viewsets.ModelViewSet):
   queryset = Curso.objects.all()
   serializer_class = CursoSerializer
+  
+class MatriculasViewSet(viewsets.ModelViewSet):
+  queryset = Matricula.objects.all()
+  serializer_class = MatriculaSerializer
+  
+class ListaMatriculasEstudante(generics.ListAPIView):
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+    return queryset
+  serializer_class = ListaMatriculasEstudantesSerializer
+  
+class ListaMatriculasCurso(generics.ListAPIView):
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+    return queryset
+  serializer_class = ListaMatriculasCursoSerializer
